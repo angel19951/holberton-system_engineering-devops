87ec68526c6deefa35ef71297ac43587e0d0da31
Angel Gonzalez <2384@holbertonschool.com>
2021-05-19 03:07:39 +0000

    Fixing code for chekcs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    subscribers = requests.get('https://www.reddit.com/r/{}/about.json'
-                              .format(subreddit),
-                              headers={'User-Agent': "My-User-Agent"},
-                              allow_redirects=False)
-    if subscribers.status_code >= 300:
-        return 0
+    try:
+        subscribers = requests.get('https://www.reddit.com/r/{}/about.json'
+                                   .format(subreddit),
+                                   headers={'User-Agent': "My-User-Agent"},
+                                   allow_redirects=False)
+        if subscribers.status_code >= 300:
+            return 0
 
-    return subscribers.json().get('data').get('subscribers')
+        return subscribers.json().get('data').get('subscribers')
+    except:
+        return 0
