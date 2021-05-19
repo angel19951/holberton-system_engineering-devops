9e401903d4ebc7b926cd94b6f89af46fc3b62515
Angel Gonzalez <2384@holbertonschool.com>
2021-05-19 03:00:14 +0000

    Fixing code for checkers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    subcribers = requests.get('https://www.reddit.com/r/{}/about.json'
+    subscribers = requests.get('https://www.reddit.com/r/{}/about.json'
                               .format(subreddit),
-                              headers={'User-Agen': "My-User-Agent"},
+                              headers={'User-Agent': "My-User-Agent"},
                               allow_redirects=False)
-    if subcribers.status_code >= 300:
+    if subscribers.status_code >= 300:
         return 0
 
-    return subcribers.json().get('data').get('subscribers')
+    return subscribers.json().get('data').get('subscribers')
